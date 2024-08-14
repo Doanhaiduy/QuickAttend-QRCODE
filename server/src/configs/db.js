603ec02e3c9641445d18d9bc12fc2761b1992c7b
Doanhaiduy <haiduytbt2k3@gmail.com>
2024-08-14 23:14:43 +0700

    chore(server): setup mongo url

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+    const connectString =
+        process.env.NODE_ENV === 'production' ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_LOCAL;
     try {
-        const connect = await mongoose.connect(process.env.MONGO_URI_LOCAL);
+        const connect = await mongoose.connect(connectString);
         console.log(`Connect to database successfully: ${connect.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
